6942af7d7ecbbc885fdfa30e41c7444da1c8b9ad
Solomon-8 <914573285@qq.com>
2018-09-26 20:53:48 +0800

    Initial commit

---
+++ src/createPattern/AbstractFactory/Main.java
@@ -0,0 +1,41 @@
+package createPattern.AbstractFactory;
+
+/**
+ * @author Solomon
+ * @date 2018/9/26
+ * if you founded any bugs in my code
+ * look at my face
+ * that's a feature
+ * ─ wow ──▌▒█───────────▄▀▒▌───
+ * ────────▌▒▒▀▄───────▄▀▒▒▒▐───
+ * ───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐───
+ * ─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐───
+ * ───▄▀▒▒▒▒▒▒ such difference ─
+ * ──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌──
+ * ──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐──
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌─
+ * ─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒ electrons ▒▌
+ * ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──
+ * ──▀ amaze ▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌──
+ * ────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀───
+ * ───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀─────
+ * ──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀────────
+ */
+public class Main {
+    public static void main(String[] args) {
+        AbstractFactory factory = new FactoryOne();
+        AbstractProductA productA = factory.createProductA();
+        AbstractProductB productB = factory.createProductB();
+        productA.use();
+        productB.eat();
+        factory = new FactoryTwo();
+        productA = factory.createProductA();
+        productB = factory.createProductB();
+        productA.use();
+        productB.eat();
+    }
+}
---
+++ src/createPattern/SimpleFactoryPattern/Top.java
@@ -0,0 +1,70 @@
+package createPattern.SimpleFactoryPattern;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Solomon
+ * @date 2018/9/26
+ * if you founded any bugs in my code
+ * look at my face
+ * that's a feature
+ * ─ wow ──▌▒█───────────▄▀▒▌───
+ * ────────▌▒▒▀▄───────▄▀▒▒▒▐───
+ * ───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐───
+ * ─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐───
+ * ───▄▀▒▒▒▒▒▒ such difference ─
+ * ──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌──
+ * ──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐──
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌─
+ * ─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒ electrons ▒▌
+ * ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──
+ * ──▀ amaze ▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌──
+ * ────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀───
+ * ───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀─────
+ * ──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀────────
+ */
+abstract class Top{
+    abstract void sayHi();
+}
+
+class A extends Top{
+
+    @Override
+    void sayHi() {
+        System.out.println("hi");
+    }
+}
+
+class B extends Top{
+
+    @Override
+    void sayHi() {
+        System.out.println("hello");
+    }
+}
+
+class Main{
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException {
+        //JDK example
+        Cipher cp = Cipher.getInstance("DESede");
+        //self example
+
+    }
+
+
+    public static Top getHi(String type) throws Exception {
+        if ("A".equals(type)){
+            return new A();
+        } else if ("B".equals(type)){
+            return new B();
+        } else {
+            throw new Exception("type not found");
+        }
+    }
+}
