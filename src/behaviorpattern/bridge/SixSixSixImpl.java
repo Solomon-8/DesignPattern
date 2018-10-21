6044d1eade204a0178ff532bb9beff90cc8cb5a3
Solomon-8 <914573285@qq.com>
2018-10-21 19:33:01 +0800

    添加bridge模式

---
+++ src/behaviorpattern/bridge/SixSixSixImpl.java
@@ -0,0 +1,39 @@
+package behaviorpattern.bridge;
+
+/**
+ * @author Solomon
+ * @date 2018/10/21
+ * if you founded any bugs in my code
+ * look at my face
+ * that's a feature
+ * ─ wow ──▌▒█───────────▄▀▒▌───
+ * ────────▌▒▒▀▄───────▄▀▒▒▒▐───
+ * ───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐───
+ * ─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐───
+ * ───▄▀▒▒▒▒▒▒ such difference ─
+ * ──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌──
+ * ──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐──
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌─
+ * ─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒ electrons ▒▌
+ * ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──
+ * ──▀ amaze ▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌──
+ * ────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀───
+ * ───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀─────
+ * ──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀────────
+ * " "
+ */
+public class SixSixSixImpl implements SixSixSixService {
+    @Override
+    public void sixsixsix() {
+        System.out.println("six six six!");
+    }
+
+    @Override
+    public void nice() {
+        System.out.println("six six six,nice!");
+    }
+}
